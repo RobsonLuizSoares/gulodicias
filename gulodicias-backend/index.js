93173755f05b84a5f9d43708bc4c7516a588ced2
Robson Luiz <robsonptrainer@gmail.com>
2021-04-03 17:56:06 -0300

    alternate keys fixed

--- gulodicias-backend/index.js
+++ gulodicias-backend/index.js
@@ -6,8 +6,8 @@ const fs = require('fs')
 const app = require('./app')
 const options = {
     //tls
-    key: fs.readFileSync('/etc/letsencrypt/live/api-gulodicias.liberty.app.br/fullchain.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/api-gulodicias.liberty.app.br/privkey.pem'),
+    key: fs.readFileSync('/etc/letsencrypt/live/api-gulodicias.liberty.app.br/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/api-gulodicias.liberty.app.br/fullchain.pem'),
     //mtls
     ca: fs.readFileSync('./ca-gerencianet.crt'), // gerencianet
     minVersion: 'TLSv1.2',
