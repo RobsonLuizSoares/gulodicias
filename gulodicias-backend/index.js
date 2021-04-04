af27263b3799e62dcaa698b4ef2e5d406f83dd2b
Robson Luiz <robsonptrainer@gmail.com>
2021-04-04 16:27:54 -0300

    open server again

--- gulodicias-backend/index.js
+++ gulodicias-backend/index.js
@@ -20,16 +20,14 @@ const options = {
 
 
 const server = https.createServer(options, app)
-server.close(() => {
-    console.log('server closed')
-})
-/*server.listen(443, () => {
+
+server.listen(443, () => {
     console.log('Server Running')
     console.log('Create webhook for pix')
     createWebhook().then(() => {
         console.log('webhook created')
     })
-}) */
+})
 
 
 
