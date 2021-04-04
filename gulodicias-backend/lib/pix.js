192a47ff7f24890832f4ba61b6d294f57489927b
Robson Luiz <robsonptrainer@gmail.com>
2021-04-04 18:31:54 -0300

    pix 12

--- gulodicias-backend/lib/pix.js
+++ gulodicias-backend/lib/pix.js
@@ -116,7 +116,7 @@ const createPixCharge = async () => {
             nome: 'Robson Luiz'
         },
         valor: {
-            original: '0.14',
+            original: '0.12',
         },
         chave: keyPix,
         solicitacaoPagador: 'Cobrança das docuras',
